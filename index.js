d756ed997477ef3fe2b723f4ca738aa013be6e96
Arman <erarmanonemail@gmail.com>
2025-09-03 07:01:16 +0530

    mongodb atlas

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/urlshortener", {
+mongoose.connect("mongodb+srv://myAppAdmin:arman1234%40@cluster0.cknckme.mongodb.net/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
